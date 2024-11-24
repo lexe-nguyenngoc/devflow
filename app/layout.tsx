a4f271d3ec249ec10cdad293d808278632b90d8b
Lexe <ngocle.ng1001@gmail.com>
2024-11-25 05:07:46 +0700

    feat: setup theme

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import React from "react";
 
+import ThemeProvider from "@/context/Theme";
+
 import "./globals.css";
 
 const inter = localFont({
@@ -31,11 +33,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body
         className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
       >
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );

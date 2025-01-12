8d6d15850d2f6e5eebb7015689ce4a175572306f
Lexe <ngocle.ng1001@gmail.com>
2025-01-12 11:56:35 +0700

    feat: implement RightSideBar component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,16 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+import { TECH_MAP } from "@/constants/techMap";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getDeviconClassName = (techName: string) => {
+  const normalizedTechName = techName.replace(/[ .]/g, "").toLowerCase();
+
+  const iconClass = TECH_MAP[normalizedTechName];
+
+  return iconClass ? `${iconClass} colored` : "devicon-devicon-plain";
+};

cb1d4986a231e310cb96ff27e1a358604555d457
Lexe <ngocle.ng1001@gmail.com>
2025-01-29 05:44:45 +0700

    feat: implement home filter

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 
+import HomeFilter from "@/components/filters/HomeFilter";
 import LocalSearch from "@/components/search/LocalSearch";
 import { Button } from "@/components/ui/button";
 import ROUTES from "@/constants/routes";
@@ -9,10 +10,7 @@ const questions = [
     _id: "1",
     title: "How to learn React?",
     description: "I want to learn React,  can anyone help me?",
-    tags: [
-      { _id: "1", name: "React" },
-      { _id: "2", name: "Javascript" },
-    ],
+    tags: [{ _id: "1", name: "Javascript" }],
     author: { _id: "1", name: "Lexe" },
     upvote: 10,
     answers: 5,
@@ -40,11 +38,21 @@ interface SearchParams {
 }
 
 const Home = async ({ searchParams }: SearchParams) => {
-  const { query = "" } = await searchParams;
+  const { query = "", filter = "" } = await searchParams;
 
-  const filteredQuestions = questions.filter((question) =>
-    question.title.toLowerCase().includes(query?.toLowerCase()),
-  );
+  const filteredQuestions = questions.filter((question) => {
+    const matchesQuery = question.title
+      .toLowerCase()
+      .includes(query?.toLocaleLowerCase());
+
+    const matchesFilter = filter
+      ? question.tags.some(
+          (tag) => tag.name.toLowerCase() === filter?.toLowerCase(),
+        )
+      : true;
+
+    return matchesQuery && matchesFilter;
+  });
 
   return (
     <>
@@ -62,7 +70,7 @@ const Home = async ({ searchParams }: SearchParams) => {
           otherClasses="flex-1"
         />
       </section>
-
+      <HomeFilter />
       <div className="mt-10 flex w-full flex-col gap-6">
         {filteredQuestions.map((question) => (
           <h1 key={question._id}>{question.title}</h1>

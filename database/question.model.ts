98c42b0edf73ed48d63e32c910f30f31ae2ce044
Lexe <ngocle.ng1001@gmail.com>
2025-01-30 23:54:54 +0700

    feat: implement question model

---
+++ database/question.model.ts
@@ -0,0 +1,30 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export interface IQuestion {
+  title: string;
+  content: string;
+  tags: Types.ObjectId[];
+  views: number;
+  upvote: number;
+  downvote: number;
+  answers: number;
+  author: Types.ObjectId;
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    tags: [{ type: Schema.Types.ObjectId, ref: "Tag" }],
+    views: { type: Number, default: 0 },
+    upvote: { type: Number, default: 0 },
+    downvote: { type: Number, default: 0 },
+    answers: { type: Number, default: 0 },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  },
+  { timestamps: true },
+);
+
+const Question =
+  models?.Account || model<IQuestion>("Question", QuestionSchema);
+export default Question;

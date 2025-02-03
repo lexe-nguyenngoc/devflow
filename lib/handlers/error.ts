e1f50cfa0a564f7d725e0285077824fe09244058
Lexe <ngocle.ng1001@gmail.com>
2025-02-03 15:35:31 +0700

    feat: implement Logging

--- lib/handlers/error.ts
+++ lib/handlers/error.ts
@@ -2,6 +2,7 @@ import { NextResponse } from "next/server";
 import { ZodError } from "zod";
 
 import { RequestError, ValidationError } from "../http-error";
+import logger from "../logger";
 
 export type ResponseType = "api" | "server";
 
@@ -23,6 +24,10 @@ const formatResponse = (
 
 const handleError = (error: unknown, responseType: ResponseType = "server") => {
   if (error instanceof RequestError) {
+    logger.error(
+      { err: error },
+      `${responseType.toUpperCase()} Error : ${error.message}`,
+    );
     return formatResponse(
       responseType,
       error.statusCode,
@@ -36,6 +41,11 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
       error.flatten().fieldErrors as Record<string, string[]>,
     );
 
+    logger.error(
+      { err: error },
+      `Validation Error : ${validationError.message}`,
+    );
+
     return formatResponse(
       responseType,
       validationError.statusCode,
@@ -45,8 +55,12 @@ const handleError = (error: unknown, responseType: ResponseType = "server") => {
   }
 
   if (error instanceof Error) {
+    logger.error(error.message);
     return formatResponse(responseType, 500, error.message);
   }
+
+  logger.error({ err: error }, "An unexpected error occurred.");
+  return formatResponse(responseType, 500, "An unexpected error occurred.");
 };
 
 export default handleError;

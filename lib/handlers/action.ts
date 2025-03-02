2cf146d2d203647734cb3ad0f5b4553338337245
Lexe <ngocle.ng1001@gmail.com>
2025-03-02 23:34:16 +0700

    feat: implement action handler

---
+++ lib/handlers/action.ts
@@ -0,0 +1,48 @@
+"use server";
+
+import { Session } from "next-auth";
+import { ZodError, ZodSchema } from "zod";
+
+import { auth } from "@/auth";
+
+import { UnauthorizedError, ValidationError } from "../http-error";
+import dbConnect from "../mongoose";
+
+type ActionOptions<T> = {
+  params?: T;
+  schema?: ZodSchema<T>;
+  authorize?: boolean;
+};
+
+async function action<T>({
+  params,
+  schema,
+  authorize = false,
+}: ActionOptions<T>) {
+  if (schema && params) {
+    try {
+      schema.parse(params);
+    } catch (error) {
+      if (error instanceof ZodError) {
+        return new ValidationError(
+          error.flatten().fieldErrors as Record<string, string[]>,
+        );
+      }
+
+      return new Error("Schema validation failed");
+    }
+  }
+
+  let session: Session | null = null;
+  if (authorize) {
+    session = await auth();
+
+    if (!session) return new UnauthorizedError();
+  }
+
+  await dbConnect();
+
+  return { params, session };
+}
+
+export default action;

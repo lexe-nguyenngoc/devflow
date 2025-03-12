c2ed91dec50adeeb6cc3880ad2f019cef77265f7
Lexe <ngocle.ng1001@gmail.com>
2025-03-12 18:54:57 +0700

    feat: implement Question Detail View #2

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -7,15 +7,20 @@ import Preview from "@/components/editor/Preview";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
-import { getQuestion } from "@/lib/actions/question.action";
+import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
-
-import View from "../view/page";
 
 const QuestionDetails = async ({ params }: RouteParams) => {
   const { id } = await params;
 
-  const { success, data: question } = await getQuestion({ questionId: id });
+  // eslint-disable-next-line no-unused-vars, @typescript-eslint/no-unused-vars
+  const [_, { success, data: question }] = await Promise.all([
+    incrementViews({ questionId: id }),
+    getQuestion({ questionId: id }),
+  ]);
+  // await incrementViews({ questionId: id });
+
+  // const { success, data: question } = await getQuestion({ questionId: id });
 
   if (!success || !question) return redirect("/404");
 
@@ -23,7 +28,6 @@ const QuestionDetails = async ({ params }: RouteParams) => {
 
   return (
     <>
-      <View questionId={id} />
       <div className="flex-start w-full flex-col">
         <div className="flex w-full flex-col-reverse justify-between">
           <div className="flex items-center justify-start gap-1">

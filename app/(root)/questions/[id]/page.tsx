7e3f877647e1b85b8a3e16a59b900db890f81408
Lexe <ngocle.ng1001@gmail.com>
2025-03-15 11:53:44 +0700

    feat: implement Get All Answers action

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -9,6 +9,7 @@ import AnswerForm from "@/components/forms/AnswerForm";
 import Metric from "@/components/Metric";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
+import { getAnswers } from "@/lib/actions/answer.action";
 import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
 
@@ -19,6 +20,18 @@ const QuestionDetails = async ({ params }: RouteParams) => {
 
   const { success, data: question } = await getQuestion({ questionId: id });
   if (!success || !question) return redirect("/404");
+  const {
+    success: areAnswerLoaded,
+    data: answersResult,
+    error: answerError,
+  } = await getAnswers({
+    questionId: question._id,
+    page: 1,
+    pageSize: 10,
+    filter: "latest",
+  });
+
+  console.log("ANSWERS", answersResult);
 
   const { title, author, createdAt, answers, views, tags, content } = question;
 

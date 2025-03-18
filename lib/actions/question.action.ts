199829b77255b1c318ab7581be4781230576c82c
Lexe <ngocle.ng1001@gmail.com>
2025-03-19 06:31:55 +0700

    feat: implement HotQuestions on RightSideBar

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -15,6 +15,7 @@ import {
   IncrementViewsSchema,
   PaginatedSearchParamsSchema,
 } from "../validations";
+import dbConnect from "../mongoose";
 
 export async function createQuestion(
   params: CreateQuestionParams,
@@ -304,6 +305,20 @@ export async function incrementViews(
     await question.save();
 
     return { success: true, data: { views: question.views } };
+  } catch (error) {
+    return handleError(error) as unknown as ErrorResponse;
+  }
+}
+
+export async function getHotQuestions(): Promise<ActionResponse<Question[]>> {
+  try {
+    await dbConnect();
+
+    const questions = await Question.find()
+      .sort({ views: -1, upvotes: -1 })
+      .limit(5);
+
+    return { success: true, data: JSON.parse(JSON.stringify(questions)) };
   } catch (error) {
     return handleError(error) as unknown as ErrorResponse;
   }

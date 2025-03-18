b13ce17dc7b4d8b82ceca0687da40b6ef61ecaaa
Lexe <ngocle.ng1001@gmail.com>
2025-03-19 00:04:57 +0700

    feat: integrate Filters Everywhere

--- app/(root)/questions/[id]/page.tsx
+++ app/(root)/questions/[id]/page.tsx
@@ -18,9 +18,9 @@ import { getQuestion, incrementViews } from "@/lib/actions/question.action";
 import { hasVoted } from "@/lib/actions/vote.action";
 import { formatNumber, getTimeStamp } from "@/lib/utils";
 
-const QuestionDetails = async ({ params }: RouteParams) => {
+const QuestionDetails = async ({ params, searchParams }: RouteParams) => {
   const { id } = await params;
-
+  const { page, pageSize, filter } = await searchParams;
   after(incrementViews({ questionId: id }));
 
   const { success, data: question } = await getQuestion({ questionId: id });
@@ -32,9 +32,9 @@ const QuestionDetails = async ({ params }: RouteParams) => {
     error: answerError,
   } = await getAnswers({
     questionId: question._id,
-    page: 1,
-    pageSize: 10,
-    filter: "latest",
+    page: Number(page) || 1,
+    pageSize: Number(pageSize) | 10,
+    filter,
   });
 
   const hasVotedPromise = hasVoted({
